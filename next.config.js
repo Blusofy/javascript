9b08c0bf717623604c8e8ea26b899ae0dd219787
Muhammad Minhaj <developer.mdminhaj@gmail.com>
2021-05-06 00:59:06 +0600

    Creating for push Js content

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+    env: {
+        C_NAME: 'https://informativecoding.github.io',
+        ROOT_PAGE: '/javascript',
+        GITHUB_CONTENT_API_URI: 'https://api.github.com/repos/InformativeCoding/data/contents',
+        GITHUB_AUTH_API_TOKEN: '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',
+    },
+    assetPrefix: isProd ? '/javascript' : '',
+    basePath: isProd ? '/javascript' : '',
+};
